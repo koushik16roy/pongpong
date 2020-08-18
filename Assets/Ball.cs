68c433322b31e4185a315b896c7a2912ea4302e6
Koushik Roy <66838109+koushik16roy@users.noreply.github.com>
2020-08-18 23:37:12 +0530

    addd

---
+++ Assets/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    Vector2 direction;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.one.normalized;  // (1,1)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = direction * speed;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("wall"))
+        {
+            direction.y = -direction.y;
+        }
+        if(collision.gameObject.CompareTag("player"))
+        {
+            speed += Random.Range(1f, 2f);
+            direction.x = -direction.x;
+        }
+    }
+}
--- Assets/player.cs
+++ Assets/player.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public float speed;
+    public float minY;
+    public float maxY;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.name == "player2")
+        {
+            MovePlayer(gameObject.name);
+        }
+        else if (gameObject.name == "player")
+        {
+            MovePlayer(gameObject.name);
+        }
+    }
+    void MovePlayer(string player)
+    {
+        float moveY = Input.GetAxis(player) * speed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x,Mathf.Clamp( transform.position.y + moveY,minY,maxY ));
     }
 }
